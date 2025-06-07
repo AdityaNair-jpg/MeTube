95a3714d3216d190c8adf4f0a94f703af7beeb0e
AdityaNair-jpg <adityanair807@gmail.com>
2025-06-07 23:04:08 +0530

    second commit

---
+++ src/app/[home]/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image src="/mtlogo.png" height={50} width={50} alt="Logo" />
+      <p className="text-2xl font-bold tracking-tight">MeTube2</p>
+    </div>
+  );
+}
--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,7 +1,7 @@
 export const HomeNavbar = () => {
     return (
-        <div>
+        <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md px-2 pr-5 ">
             Home Navbar
-        </div>
+        </nav>
     );
 };
