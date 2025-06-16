4349f4994b29f5ae571a7970ce3804ac932bd804
AdityaNair-jpg <adityanair807@gmail.com>
2025-06-17 00:03:47 +0530

    tRPC Configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,6 @@
+// src/db/index.ts
 import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres'; // This is required
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const queryClient = postgres(process.env.DATABASE_URL!); // initialize the SQL client
+export const db = drizzle(queryClient); // pass client to drizzle
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,12 +1,22 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 import { cache } from 'react';
+import { eq } from "drizzle-orm";
+import { users } from "@/db/schema";
+import { auth } from "@clerk/nextjs/server";
 import superjson from "superjson";
-export const createTRPCContext = cache(async () => {
-    
+import { db } from '@/db';
+import { ratelimit } from '@/lib/ratelimit';
 
+export const createTRPCContext = cache(async () => {
+    const { userId } = await auth();
+
+    return { clerkUserId: userId};
 });
 
-const t = initTRPC.create({
+
+export type Context = Awaited <ReturnType<typeof createTRPCContext>>;
+
+const t = initTRPC.context<Context>().create({
 
     transformer: superjson,
 });
@@ -14,3 +24,35 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+
+
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts){
+    const { ctx } = opts;
+
+    if (!ctx.clerkUserId) {
+        throw new TRPCError({ code: "UNAUTHORIZED" })
+    }
+
+    const [user] = await db
+        .select()
+        .from(users)
+        .where(eq(users.id, ctx.clerkUserId))
+        .limit(1);
+
+    if (!user) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    const { success } = await ratelimit.limit(user.id);
+
+    if(!success){
+        throw new TRPCError({ code: "TOO_MANY_REQUESTS"})
+    }
+    return opts.next({
+        ctx: {
+            ...ctx,
+            user,
+        },
+    });
+});
